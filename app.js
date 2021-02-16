4831c436ae4a6f29a8bb7a40b7a1d343359050a3
abodesigner <mohammed.abosalim@gmail.com>
2021-02-16 18:07:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+// setup template engine
+app.set('view engine','ejs');
+
+//app.set('views', 'views');
+
+// setup static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res)=> {
+    res.render('index', {
+        title: 'Homepage',
+        name: 'aboanas'
+    });
+})
+
+app.get('/help', (req,res)=> {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'abosalem'
+    });
+})
+
+app.get("/weather", (req,res)=>{
+console.log(req.query.address);
+    if (!req.query.address){
+        return res.send({
+            error: 'No address provided yet'
+        })
+    }
+    res.send({
+        location: 'Tanta',
+        forecast: "55",
+        address: req.query.address
+    })
+})
+
+app.get("/products", (req,res)=>{
+
+    if (!req.query.search) {
+        return res.send({
+            error: 'you must provide search term'
+        })
+    }
+    console.log(req.query.search);
+    res.send({
+        products: []
+    })
+})
+
+app.get("*", (req,res)=>{
+    res.send('404 NOT FOUND PAGE')
+})
+
+app.listen(3000, ()=> {
+    console.log("Server running ...");
+})
---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+// console.log("file loaded in browser successully");
+
+// fetch("http://puzzle.mead.io/puzzle").then(response=>{
+//     response.json().then(data=>{
+//         console.log(data);
+//     })
+// })
+
+
+
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const location = search.value;
+    fetch(`http://localhost:3000/weather?address=${location}`).then(response=>{
+    response.json().then(data=>{
+        if(data.error) {
+            console.log(data.error);
+        } else {
+            console.log("Forcast:" , data.forecast);
+        }
+    })
+})
+})
